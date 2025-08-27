a567d5e4cb25b9845b37565ba00d064058f0feed
Hardik Parmar <hardikpersonal11@gmail.com>
2025-08-27 22:34:30 +0530

    adding frontend files

--- src/utils/APIRoutes.js
+++ src/utils/APIRoutes.js
@@ -1,4 +1,4 @@
-export const host = "https://backend-flame-xi-63.vercel.app/";
+export const host = "https://backend-1-71rl.onrender.com";
 export const registerRoute = `${host}/api/auth/register`;
 export const loginRoute = `${host}/api/auth/login`;
 export const SetAvatarRoute = `${host}/api/auth/SetAvatar`;
